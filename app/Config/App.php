6f4299f8e6b380002f12aa903a0023c40479cf88
Rizki <rizkiramadhan4848@gmail.com>
2023-10-19 21:15:37 +0700

    init push

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,8 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://goro.goro.docker';
+    // public string $baseURL = 'https://goro.goro.docker';
+    public string $baseURL = 'https://main--inquisitive-babka-c65366.netlify.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
